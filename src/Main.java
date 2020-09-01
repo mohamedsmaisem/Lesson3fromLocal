ad98ef03a6f5f1c8c13d9d00f9a4610126676c51
mohmedsmaisem <mohmed2smaisem@icloud.com>
2020-09-01 20:46:00 +0200

    a program that gets a number and prints this pattern

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hhhh");
+      printnummbe(5);
+    }
+    public static void printnummbe(int n){
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
     }
 }
